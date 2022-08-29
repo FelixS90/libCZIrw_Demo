12c83e4d96e66365925d3a246292f048fdeb31ba
Scheffler, Felix <felix.scheffler@zeiss.com>
2022-08-29 16:21:41 +0200

    Initial commit

---
+++ Src/libCZI_UnitTests/MemOutputStream.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "inc_libCZI.h"
+
+class CMemOutputStream : public libCZI::IOutputStream
+{
+private:
+	char* ptr;
+	size_t allocatedSize;
+	size_t usedSize;
+public:
+	CMemOutputStream(size_t initialSize);
+	virtual ~CMemOutputStream() override;
+
+	const char* GetDataC() const
+	{
+		return this->ptr;
+	}
+
+	size_t GetDataSize() const
+	{
+		return this->usedSize;
+	}
+
+	std::shared_ptr<void> GetCopy(size_t* pSize) const
+	{
+		auto spBuffer = std::shared_ptr<void>(malloc(this->usedSize), [](void* p)->void {free(p); });
+		memcpy(spBuffer.get(), this->ptr, this->usedSize);
+		if (pSize != nullptr)
+		{
+			*pSize = this->usedSize;
+		}
+
+		return spBuffer;
+	}
+public:	// interface IOutputStream
+	virtual void Write(std::uint64_t offset, const void* pv, std::uint64_t size, std::uint64_t* ptrBytesWritten) override;
+private:
+	void EnsureSize(std::uint64_t newSize);
+};
