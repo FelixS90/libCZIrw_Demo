e9940d0014dbc6d18bf6c6d89729cf8dcbfd66b0
Scheffler, Felix <felix.scheffler@zeiss.com>
2022-09-29 19:28:50 +0200

    Fix strange syntax error

--- Src/libCZI_UnitTests/test_metadatabuilder.cpp
+++ Src/libCZI_UnitTests/test_metadatabuilder.cpp
@@ -1,8 +1,6 @@
 // SPDX-FileCopyrightText: 2017-2022 Carl Zeiss Microscopy GmbH
 //
-// SPDX-License-Identifier: LGPL-3.0-or-later
-
-﻿
+// SPDX-License-Identifier: LGPL-3.0-or-later﻿
 #include "pch.h"
 #include "inc_libCZI.h"
 
